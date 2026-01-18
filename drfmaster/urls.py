50a9b4bfc4ead278db772516bfd22bee1c17df28
abuaminuu <i.abuaminu@gmail.com>
2026-01-18 21:49:02 +0100

    add test url in drfmaster urls py

--- drfmaster/urls.py
+++ drfmaster/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view as swagger_get_schema_view
+from django.http import HttpResponse
 
 schema_view = swagger_get_schema_view(
     openapi.Info(
@@ -32,6 +33,7 @@ schema_view = swagger_get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger-schema-ui"),
+    path("test/", lambda request: HttpResponse("Test OK")),
     path("api/", include('snippets.urls')),
     path("api-auth/", include("rest_framework.urls")),
 ]
